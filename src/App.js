188f670432236f18faf15208c004d7df49d86ab8
Knderson21 <knderson21@gmail.com>
2024-07-14 16:00:02 -0400

    component for zooming in and out of image

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import ZoomableImage from "./ZoomableImage.js"
 
-export default App;
+export default function App() {
+  return <div>
+    <ZoomableImage imageURL="https://miro.medium.com/v2/resize:fit:1400/format:webp/1*0tGN1LpQasvzmMkg_5CNQg.png" zoomFactor={10} />
+  </div>;
+}
